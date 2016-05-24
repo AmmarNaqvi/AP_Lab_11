d044ee6ed5df8167961479683f371fc391522188
AmmarNaqvi <13besesabbas@seecs.edu.pk>
2016-05-24 23:27:42 +0500

    first commit

---
+++ lab11.scm
@@ -0,0 +1,13 @@
+;Adanced Programming Lab 11
+;Syed Ammar Abbas Naqvi
+;Reg # 6046
+
+
+(define (if m)
+  (f (m m)) (f (m m)))
+
+(define (loop m)
+  ((isZero m)0 add f(m-1) m))
+
+(define (aCompletelyUselessFunctionCreatedOnlyToMakeYourLifeMiserable m n)
+(or (pred m n) (isZero m) (if loop n) (add m n)))
